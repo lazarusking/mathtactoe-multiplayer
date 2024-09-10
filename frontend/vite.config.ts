c9efe2e2391e52c2124e6914e4cddaafd78465db
Lazarus <vegetasssuper@gmail.com>
2024-09-10 12:54:52 +0000

    add compression

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
+import compression from 'vite-plugin-compression2'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    compression({ include: /\.(html|xml|js|css|json|txt|ico|svg|gif|webp|ttf)$/ })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
